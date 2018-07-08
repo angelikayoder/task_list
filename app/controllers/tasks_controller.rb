fa30178f8d41a12f835fa492886b1edb07296417
angelikayoder <angelika.yoder@gmail.com>
2018-07-08 13:31:22 -0400

    Routes to layouts & views, bootstrap cdn

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,89 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  # GET /tasks
+  # GET /tasks.json
+  def index
+    @tasks = Task.all
+  end
+
+  # GET /tasks/1
+  # GET /tasks/1.json
+  def show
+  end
+
+  # GET /tasks/new
+  # if a position is created, makes it equal to max index + 1. If nil, makes it 1
+  def new
+    @task = Task.new(position: current_position)
+  end
+
+  # GET /tasks/1/edit
+  def edit
+  end
+
+  # POST /tasks
+  # POST /tasks.json
+  def create
+    @task = Task.new(task_params)
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to @task, notice: 'Task was successfully created.' }
+        format.json { render :show, status: :created, location: @task }
+      else
+        format.html { render :new }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /tasks/1
+  # PATCH/PUT /tasks/1.json
+  def update
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
+        format.json { render :show, status: :ok, location: @task }
+      else
+        format.html { render :edit }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /tasks/1
+  # DELETE /tasks/1.json
+  def destroy
+    @task.destroy
+    respond_to do |format|
+      format.html { redirect_to tasks_url, notice: 'Task was successfully deleted.' }
+      format.json { head :no_content }
+    end
+  end
+
+  def complete
+    @task.update_attribute(:is_done, true)
+    redirect_to @tasks, notice: "Task is done!"
+  end
+
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def current_position
+      return 0 if Task.all.empty?
+      Task.maximum('position') + 1
+    end
+
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def task_params
+      params.require(:task).permit(:position, :title, :is_done)
+    end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks do
+    member do
+      patch :complete
+    end
+  end
+  root 'tasks#index'
 end
